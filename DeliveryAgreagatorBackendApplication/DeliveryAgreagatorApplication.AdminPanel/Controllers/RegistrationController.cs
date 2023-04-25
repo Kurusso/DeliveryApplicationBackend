98ed18e55334edbd9cc2f5c906fdfdcee19fb5d8
Alexey <lesha.braun1029@gmail.com>
2023-04-25 19:07:18 +0700

    add mvc project

---
+++ DeliveryAgreagatorBackendApplication/DeliveryAgreagatorApplication.AdminPanel/Controllers/RegistrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryAgreagatorApplication.AdminPanel.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
